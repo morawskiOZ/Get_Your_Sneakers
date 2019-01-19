005108acb8fbb03df1cd7d20340e04e953b757c2
Piotr Morawski <piotrmorawski90@gmail.com>
2019-01-19 17:36:51 +0100

    add App wrapper

---
+++ frontend/pages/_app.js
@@ -0,0 +1,17 @@
+
+import App, {Container} from 'next/app';
+
+class MyApp extends App {
+    render(){
+        const {Component} = this.props
+
+        return (
+            <Container>
+                <p>Hey I am on every page</p>
+                <Component />
+            </Container>
+        )
+    }
+}
+
+export default MyApp;
