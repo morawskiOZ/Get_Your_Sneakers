3991d4525c408d6b15179491e6d85be97e1f767e
Piotr Morawski <piotrmorawski90@gmail.com>
2019-02-03 21:21:23 +0100

    add mutation and query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    items: forwardTo('db')
+// async items(parent, args, ctx, info) {
+//     console.log('getting items');
+//     const item = await ctx.db.query.items()
+//     return item
+//     }
+};
 
 module.exports = Query;
