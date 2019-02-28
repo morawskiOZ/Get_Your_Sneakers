40cc7c6929482dc7eeabaf49f24ce96b0a6128b5
Piotr Morawski <piotrmorawski90@gmail.com>
2019-02-28 08:53:30 +0100

    add update item

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,12 +1,13 @@
-const { forwardTo } = require('prisma-binding');
+const { forwardTo } = require("prisma-binding");
 
-const Query = {
-    items: forwardTo('db')
-// async items(parent, args, ctx, info) {
-//     console.log('getting items');
-//     const item = await ctx.db.query.items()
-//     return item
-//     }
+const Query = { 
+  items: forwardTo("db"),
+  item: forwardTo("db")
+  // async items(parent, args, ctx, info) {
+  //     console.log('getting items');
+  //     const item = await ctx.db.query.items()
+  //     return item
+  //     }
 };
 
 module.exports = Query;
